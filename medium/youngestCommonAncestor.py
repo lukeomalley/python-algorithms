90cbdbb6aeae50787752756e871289e0451e806f
Luke O'Malley <Lukeomalley95@gmail.com>
2019-10-14 11:47:38 -0400

    solved youngestCommonAncestor

--- medium/youngestCommonAncestor.py
+++ medium/youngestCommonAncestor.py
@@ -1,21 +1,38 @@
 # You're given three inputs, all of which are instances of a class that have an "ancestor" property pointing to their youngest ancestor. The first input is the top ancestor in an ancestral tree (i.e., the only instance that has no ancestor), and the other two inputs are descendants in the ancestral tree. Write a function that returns the youngest common ancestor to the two descendants.
 class AncestralTree:
-    def __init__(self, name):
-        self.name = name
-        self.ancestor = None
+	def __init__(self, name):
+		self.name = name
+		self.ancestor = None
 
-    def addAsAncestor(self, descendants):
-        for descendant in descendants:
-            descendant.ancestor = self
+	def addAsAncestor(self, descendants):
+		for descendant in descendants:
+			descendant.ancestor = self
 
-
+# O(d) TIME | O(1) SPACE
 def getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo):
-  return 0
+	depthOne = getDescendantDepth(descendantOne, topAncestor)
+	depthTwo = getDescendantDepth(descendantTwo, topAncestor)
 
+	if depthOne > depthTwo:
+		return backtrackAncestralTree(descendantOne, descendantTwo, depthOne - depthTwo)
+	else:
+		return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo - depthOne)
 
+def getDescendantDepth(descendant, topAncestor):
+	depth = 0
+	while descendant != topAncestor:
+		depth += 1
+		descendant = descendant.ancestor
+	return depth
 
-
-
+def backtrackAncestralTree(lowerDescendant, higherDescendant, diff):
+	while diff > 0:
+		lowerDescendant = lowerDescendant.ancestor
+		diff -= 1
+	while lowerDescendant != higherDescendant:
+		lowerDescendant = lowerDescendant.ancestor
+		higherDescendant = higherDescendant.ancestor
+	return lowerDescendant
 
 
 
