5cb9d241c65f3d8e5bb8ea2afeac76f6b90adaaa
Luke O'Malley <Lukeomalley95@gmail.com>
2019-10-25 11:20:42 -0400

    solved powerset recursively

--- medium/powerset.py
+++ medium/powerset.py
@@ -10,7 +10,20 @@ def powerset(array):
   for element in array:
     for i in range(len(subsets)):
       currentSubset = subsets[i]
-      subsets.append(currentSubset + element)
+      subsets.append(currentSubset + [element])
 
   return subsets
 
+def powersetRecursive(array, idx = None):
+  if idx is None:
+    idx = len(array) - 1
+  elif idx < 0:
+    return [[]]
+  element = array[idx]
+  subsets = powersetRecursive(array, idx - 1)
+
+  for i in range(len(subsets)):
+    currentSubset = subsets[i]
+    subsets.append(currentSubset + [element])
+
+  return subsets
